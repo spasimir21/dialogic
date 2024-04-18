439987d36fa04b5b917d23f49c95dbb13d83eaff
Dimitar Pavlov <mitko5450@gmail.com>
2024-04-18 22:22:55 +0300

    Added topic search input in Trending

--- frontend/pages/Trending.tsx
+++ frontend/pages/Trending.tsx
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 export default function Trending() {
+  const [topicInput, setTopicInput] = useState('');
+  const inputDelaySec = 1;
+  let timeout = useRef<any>();
+
+  function handleInput(event: any) {
+    clearTimeout(timeout.current);
+    setTopicInput(event.target.value);
+    timeout.current = setTimeout(() => {
+      event.target.value ? console.log(`Search topics for ${event.target.value}`) : null;
+    }, inputDelaySec * 1000);
+  }
+
   return (
     <div>
       <div className='w-[220px] h-usable-screen bg-[#E7E4E4] [box-shadow:_3px_0_3px_rgba(0,0,0,0.4)] flex flex-col items-center pl-2'>
-        <div className='w-[180px] h-[30px] bg-[white]  rounded-[5px] [box-shadow:_2px_2px_2px_rgba(0,0,0,0.3)] mt-3'></div>
+        <input
+          className='w-[180px] h-[30px] bg-[white]  rounded-[5px] [box-shadow:_2px_2px_2px_rgba(0,0,0,0.3)] mt-3 outline-none pl-[5px]'
+          onChange={handleInput}
+          value={topicInput}
+          placeholder='Search topics...'
+        />
         <div className='w-[180px] mt-3 mb-3  flex flex-row flex-wrap gap-2'>
           {/* Selected categories */}
           <div className='w-[130px] h-[30px] bg-[#E87331]  rounded-[5px] [box-shadow:_2px_2px_2px_rgba(0,0,0,0.3)]'></div>

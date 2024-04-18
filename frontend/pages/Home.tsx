a44bccd9e42668f53a442ae6a4dc2638a4310df4
spasimir21 <spasimir.pavlov@gmail.com>
2024-04-18 22:14:39 +0300

    added a category test to home

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,8 +1,30 @@
 import { useUser } from '@libs/client/hooks/useUser';
+import { useRequest } from '@libs/client/requestr';
+import { requests } from '@frontend/api/requests';
 import React from 'react';
 
 export default function Home() {
   const userData = useUser();
 
-  return <h3>{userData == null ? "You're not logged in!" : userData.fullname}</h3>;
+  // useRequest(requests.category.ensureCategories, {
+  //   onResult: console.log,
+  //   initialParams: {
+  //     names: ['politics', 'alobg', 'boiko', 'kon', 'operacionni', 'softuer', 'amg paketche']
+  //   }
+  // });
+
+  const categoriesRequest = useRequest(requests.category.searchCategories);
+
+  return (
+    <>
+      <h3>{userData == null ? "You're not logged in!" : userData.fullname}</h3>
+      {categoriesRequest.result == null ? (
+        <p>Loading...</p>
+      ) : (
+        categoriesRequest.result.map(name => <p key={name}>{name}</p>)
+      )}
+
+      <button onClick={() => categoriesRequest.send('amg')}>Send</button>
+    </>
+  );
 }

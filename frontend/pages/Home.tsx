11c776be93efcfe34614f9ec0392baf2d5eddc15
spasimir21 <spasimir.pavlov@gmail.com>
2024-04-21 01:26:21 +0300

    Switched to using a local instance of the database

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,30 +1,12 @@
 import { useUser } from '@libs/client/hooks/useUser';
-import { useRequest } from '@libs/client/requestr';
-import { requests } from '@frontend/api/requests';
 import React from 'react';
 
 export default function Home() {
   const userData = useUser();
 
-  // useRequest(requests.category.ensureCategories, {
-  //   onResult: console.log,
-  //   initialParams: {
-  //     names: ['politics', 'alobg', 'boiko', 'kon', 'operacionni', 'softuer', 'amg paketche']
-  //   }
-  // });
-
-  const categoriesRequest = useRequest(requests.category.searchCategories);
-
   return (
     <>
-      <h3>{userData == null ? "You're not logged in!" : userData.fullname}</h3>
-      {categoriesRequest.result == null ? (
-        <p>Loading...</p>
-      ) : (
-        categoriesRequest.result.map(name => <p key={name}>{name}</p>)
-      )}
-
-      <button onClick={() => categoriesRequest.send('amg')}>Send</button>
+      <p>{userData == null ? "You're not logged in!" : userData.fullname}</p>
     </>
   );
 }
--- services/auth/auth.module.ts
+++ services/auth/auth.module.ts
@@ -6,11 +6,12 @@ import { GoogleOAuthService } from './service/google.oauth.service';
 import { SharedConfigProvider } from '@shared/server/sharedConfig';
 import { EmailAuthService } from './service/email.auth.service';
 import { AuthTokenService } from './service/auth.token.service';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { UserService } from './service/user.service';
 import { PrismaService } from '@libs/server/prisma';
 import { TokenService } from '@libs/server/token';
 import { AuthConfigProvider } from './config';
-import { Module } from '@nestjs/common';
+import { spawnSync } from 'child_process';
 
 @Module({
   controllers: [AuthHTTPController, AuthGRPCController, EmailAuthController, GoogleOAuthController],
@@ -25,6 +26,12 @@ import { Module } from '@nestjs/common';
     GoogleOAuthService
   ]
 })
-class AuthModule {}
+class AuthModule implements OnModuleInit {
+  onModuleInit() {
+    const child = spawnSync('npx', ['prisma', 'db', 'push']);
+    console.log(child.stdout.toString());
+    console.log(child.stderr.toString());
+  }
+}
 
 export { AuthModule };
